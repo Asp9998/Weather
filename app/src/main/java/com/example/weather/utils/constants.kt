2c00e17956851a579dfdf503df5b5e168d8563ed
Aryan Patel <125112628+Asp9998@users.noreply.github.com>
2025-05-24 17:50:16 -0500

    Update constants.kt

--- app/src/main/java/com/example/weather/utils/constants.kt
+++ app/src/main/java/com/example/weather/utils/constants.kt
@@ -2,12 +2,5 @@ package com.example.weather.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "ea7a9894a9b05acc4a11c7b9ebcc6725"
+    const val API_KEY = "Your Api Key here"
 }
-
-
-// to get city name
-//http://api.openweathermap.org/geo/1.0/reverse?lat=43.589&lon=-79.6441&appid=ea7a9894a9b05acc4a11c7b9ebcc6725
-
-// for user city choice
-//http://api.openweathermap.org/geo/1.0/direct?q=Victoria&limit=100&appid=ea7a9894a9b05acc4a11c7b9ebcc6725
